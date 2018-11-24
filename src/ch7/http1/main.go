143f7b8f2a92eda52a175f7f2ec9c00b23c8161b
Daniel49zhu <zhujiacheng94@163.com>
2018-11-24 21:13:40 +0800

    http2

--- src/ch7/http1/main.go
+++ src/ch7/http1/main.go
@@ -14,7 +14,7 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", db))
 }
 
-func (d dollars) Stirng() string { return fmt.Sprintf("$%.2f", d) }
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
