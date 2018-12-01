3867980d2474ca37be58a5b1f949e819b17b5d1e
Daniel49zhu <zhujiacheng94@163.com>
2018-12-01 23:36:09 +0800

    bank1

---
+++ src/ch9/bank1/bank.go
@@ -0,0 +1,22 @@
+package bank
+
+var deposits = make(chan int) //发送存款额
+var balances = make(chan int) //接收余额
+
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+
+func teller() {
+	var balance int
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		}
+	}
+}
+
+func init() {
+	go teller()
+}
