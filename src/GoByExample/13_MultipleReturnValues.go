650d5ad5519744ee277d7d151de12476e865d92c
Daniel49zhu <zhujiacheng94@163.com>
2018-12-03 23:49:18 +0800

    Golang

---
+++ src/GoByExample/13_MultipleReturnValues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//可以返回多个值的return
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
+}
