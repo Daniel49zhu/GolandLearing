1110f7f11da5da1423362f186e4e32f3009780fd
Daniel49zhu <zhujiacheng94@163.com>
2018-12-04 23:34:09 +0800

    Golang

---
+++ src/GoByExample/14_ VariadicFunctions.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//Go by Example: Variadic Functions
+//Variadic functions can be called with any number of trailing arguments.
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sum(1, 2)
+	sum(1, 2, 3)
+
+	//If you already have multiple args in a slice,
+	// apply them to a variadic function using func(slice...) like this.
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
+
+}
