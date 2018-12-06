09082f40fdbc70aa1e1b6947332edd969bb03da5
Daniel49zhu <zhujiacheng94@163.com>
2018-12-06 22:56:12 +0800

    Golang

--- src/GoByExample/25_ChannelSynchronization.go
+++ src/GoByExample/25_ChannelSynchronization.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("working....")
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+	//传输一个信号来告诉主线程执行完毕了
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go worker(done)
+	//主线程在此阻塞 直到从done通道中接收到参数才会推出
+	//如果注释掉这句，worker线程还没开启主线程就已经结束退出了
+	<-done
+}
