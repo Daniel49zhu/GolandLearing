2ad411412bb959461a1a0f972893e2f5a5d2a9a6
Daniel49zhu <zhujiacheng94@163.com>
2018-12-15 00:05:32 +0800

    Golang

--- src/GoByExample/55_ReadingFiles.go
+++ src/GoByExample/55_ReadingFiles.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -15,7 +17,7 @@ func check(e error) {
 func main() {
 	dat, err := ioutil.ReadFile("src/GoByExample/testRead.txt")
 	check(err)
-	fmt.Print(string(dat))
+	fmt.Println(string(dat))
 
 	f, err := os.Open("src/GoByExample/testRead.txt")
 	check(err)
@@ -24,4 +26,28 @@ func main() {
 	n1, err := f.Read(b1)
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
+
+	o2, err := f.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(b2))
+
+	o3, err := f.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+
+	_, err = f.Seek(0, 0)
+	check(err)
+
+	r4 := bufio.NewReader(f)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Printf("5 bytes: %s\n", string(b4))
+
+	f.Close()
 }
