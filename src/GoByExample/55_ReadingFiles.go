9aea84bdb95428ad422c4a32499ae45b991b9b94
Daniel49zhu <zhujiacheng94@163.com>
2018-12-14 23:59:40 +0800

    Golang

--- src/GoByExample/55_ReadingFiles.go
+++ src/GoByExample/55_ReadingFiles.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func check(e error) {
@@ -15,4 +16,12 @@ func main() {
 	dat, err := ioutil.ReadFile("src/GoByExample/testRead.txt")
 	check(err)
 	fmt.Print(string(dat))
+
+	f, err := os.Open("src/GoByExample/testRead.txt")
+	check(err)
+
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 }
